62faa224ed02c16b9a31dbc9ed2c061ea5dc5b62
Deepak <deepakkumarjha@outlook.com>
2020-04-16 00:58:01 +0530

    require exports and event emitters

---
+++ requireexports/D11_ModulePatterns/Patterns/oppo5.js
@@ -0,0 +1,8 @@
+var oppo5 = 'Welcome to Oppo5 Deepak Jha';
+function  Oppo5(){
+    console.log(oppo5);
+}
+
+module.exports = {
+    OPPO5:Oppo5};
+
---
+++ requireexports/D13_RequiringCoreModules/app.js
@@ -0,0 +1,5 @@
+var util = require('util');
+
+var name = 'Deepak';
+var greeting = util.format('Hello, %s', name);
+util.log(greeting);
